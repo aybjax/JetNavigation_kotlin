702e90bc0fdd269f16cb5da6dd6d478cb3010d62
aybjax <aybat93@gmail.com>
2021-07-25 05:10:14 +0600

    dsfsd

---
+++ app/src/main/java/com/aybjax/jetnavigation/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.aybjax.jetnavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.FragmentContainerView
+
+const val STAR_SIGN_ID = "STAR_SIGN_ID"
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
